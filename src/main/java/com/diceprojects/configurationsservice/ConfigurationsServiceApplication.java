ef195adafdaf7618e497867b396c6bdc05b3bb9e
MartinDiCe <jamartindice@gmail.com>
2024-02-22 11:45:28 -0300

    Configuration

---
+++ src/main/java/com/diceprojects/configurationsservice/ConfigurationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.diceprojects.configurationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diceprojects/configurationsservice/ConfigurationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diceprojects.configurationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
